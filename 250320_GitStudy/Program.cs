75a4931ff3dd3daea6614191b7ad7b4fa343c259
AsiaticRicefish <125473806+AsiaticRicefish@users.noreply.github.com>
2025-03-20 12:25:39 +0900

    Initial Visual Studio Project

---
+++ 250320_GitStudy/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _250320_GitStudy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
